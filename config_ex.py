7763ddea4bad12641fa0fe5298d96a7b6894dc0c
joelspiers15 <joelspiers15@gmail.com>
2021-11-20 18:25:00 -0500

    Update example config with examples for auto cleanup

--- config_ex.py
+++ config_ex.py
@@ -6,7 +6,7 @@ dlThreadCount = 2                                           # Number of download
 
 # Server
 server_address = 'https://mydomain.com'                     # Public facing address corresponding to your SSL certs
-bind_address = '192.168.0.20'                              # Address your host can bind to. For me this is the local ip of my server
+bind_address = '192.168.0.20'                              	# Address your host can bind to. For me this is the local ip of my server
 
 # Twitch API auth
 clientId = "t0TkCHwV8w0Axyauzq8XNZe7QC8lDo"                 # Id from Twitch dev dashboard. Not used yet
@@ -14,5 +14,19 @@ clientSecret = "W8jVCD0rRdcm6367ccqnbm266lw5iq"             # Secret from Twitch
 
 # Subscriptions
 subscriptionSecret = "9c119a2b-a003-4a6a-bc9e-674f5484702a" # Secret given to Twitch when creating your eventSub. I used a random GUID but it can be whatever string you want
+eventSubs = [
+    {
+        "channel": "Ninja",		# Channel name CASE-SENSITIVE
+        "daysToKeepVods": 10	# How long to keep vods before deleting (Use -1 to never delete)
+    },
+    {
+        "channel": "shroud",
+        "daysToKeepVods": -1	# Never delete vods
+    },
+    {
+        "channel": "TimTheTatman",
+        "daysToKeepVods": 2
+    }
+]
 
-## Note: All keys above are randomly generated in the form of real secrets. They aren't real ids or secrets.
+## Note: All keys above are randomly generated in the shape of real secrets. They aren't real ids or secrets.
